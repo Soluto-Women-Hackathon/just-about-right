f94c75ba768b08c82ac8e9177311568c5e38ba3c
Or Rosenblatt <orr@soluto.com>
2018-03-08 22:52:50 +0200

    Insert new page

---
+++ src/KnowYourRights.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './general.css';
+import './KnowYourRights.css';
+
+class KnowYourRights extends Component {
+  render() {
+    return (
+      <div>
+        <button className="SiteButton">
+          Legal Rights
+        </button>
+        <button className="SiteButton">
+          Institutes Contact Information
+        </button>
+      </div>
+    );
+  }
+}
+
+export default KnowYourRights;
