55a0ea77d0660f8ca91a8445c2badf62d1e5c143
AvishagCs <avishag22990@gmail.com>
2018-03-08 21:49:22 +0200

    start working on main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, button } from 'react';
+import logo from './judgehat.png';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Just about Right</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div className = "left-but">
+          <button color="primary" disabled={true}>111111</button>
+        </div>
+        <div className = "right-but" >
+          <button color="primary" disabled={true}>222222</button>          
+        </div>
       </div>
     );
   }
